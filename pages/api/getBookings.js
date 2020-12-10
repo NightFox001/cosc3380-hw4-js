d1d004ba4238019575fc8fd485559b984c8f4d9c
Shane <dogunbound5@gmail.com>
2020-12-10 10:54:53 -0600

    fixed getookig

--- pages/api/getBookings.js
+++ pages/api/getBookings.js
@@ -33,24 +33,24 @@ SELECT COUNT(*) AS tickets_purchased, book_id, ticket_cost, flights.flight_id, s
 				GROUP BY flights.flight_id, a.book_id, a.ticket_cost;\n
 				`, 
 				{ type: Sequelize.QueryTypes.SELECT });
-			let book_map = new Map();
+			let book_map = new Map()
 			ticket_info.forEach((e) => {
-					let book_info = {
-						ticket_cost: e.ticket_cost,
-						tickets_purchased: e.tickets_purchased,
-						scheduled_departure: e.scheduled_departure,
-						scheduled_arrival: e.scheduled_arrival,
-						arrival_airport: e.arrival_airport_id,
-						adeparture_airport: e.departure_airport_id,
-						movie: e.movie,
-						meal: e.meal
-					}
-					if (book_map.get(e.book_id) === undefined) {
-						book_map.set(e.book_id, book_info);
-					} else {
-						let l = book_map.get(e.book_id);
-						book_map.set(e.book_id, [l, book_info])						
-					}
+				let book_info = {
+					ticket_cost: e.ticket_cost,
+					tickets_purchased: e.tickets_purchased,
+					scheduled_departure: e.scheduled_departure,
+					scheduled_arrival: e.scheduled_arrival,
+					arrival_airport: e.arrival_airport_id,
+					adeparture_airport: e.departure_airport_id,
+					movie: e.movie,
+					meal: e.meal
+				}
+				if (book_map[e.book_id] === undefined) {
+					book_map[e.book_id] = []
+					book_map[e.book_id].push(book_info)
+				} else {
+					book_map[e.book_id].push(book_info)
+				}
 			});
 			console.log(book_map)
 			return res.json(book_map)
