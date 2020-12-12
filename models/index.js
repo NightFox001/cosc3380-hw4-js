4a76fe8556709343587a9159d5ca1a939fd81457
Night Fox <Johnnyh961@gmail.com>
2020-12-12 02:56:10 -0600

    trying to merge...

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize'
-
+/*
 const creds = {
   database: "COSC3380",
   username: "cosc0146",
@@ -7,7 +7,23 @@ const creds = {
   host: "code.cs.uh.edu",
   dialect: "postgres"
 }
-
+const creds = {
+  database: "COSC3380",
+  username: "cosc0210",
+  password: "",
+  host: "76.226.74.200",
+  dialect: "postgres"
+}
+*/
+const creds = {
+  database: "COSC3380",
+  username: "cosc0210",
+  password: "",
+  host: "code.cs.uh.edu",
+  dialect: "postgres"
+}
 const { database, username, password, ...config } = creds;
+
 const connection = new Sequelize(database, username, password, config);
+
 module.exports = { connection, Sequelize }
